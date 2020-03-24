edf4269e52462f574ffb0d15ca584ae831080157
Thadeu Penna <thadeu.penna@id.uff.br>
2020-03-24 17:53:15 -0300

    biblioteca

--- linalg.c
+++ linalg.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-extern double **ler(char *arquivo, int m, int n);
-extern void imprime(double **M, int m, int n) ;
+#include "linalg.h"
 
 int main(int argc, char **argv)
 {
     double **M; 
     int m,n;
 
+    
+    if (argc<4) {
+        puts("ERRO:  entre os argumentos na linha de comando. ");
+        return 1;
+    }    
     m=atoi(argv[2]);n=atoi(argv[3]); 
     M=ler(argv[1],m,n);
     imprime(M,m,n);
